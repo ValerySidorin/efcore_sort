d3b7f5e7737fb155eee706a04d540121f334e453
Afsuga Arewreh <afsuga.arewreh@mail.ru>
2020-03-11 16:37:07 +0300

    Adding main classes

---
+++ efcore_sort/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace efcore_sort.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int CompanyId { get; set; }
+
+        public Company Company { get; set; }
+    }
+}
